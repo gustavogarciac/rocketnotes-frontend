36663623a5c0d1c140dff79c15db2d4b53eabe53
gustavo <gustavogarciaci1@gmail.com>
2023-06-30 10:34:45 -0300

    first commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+export function Details(){
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo repellendus odit quidem veniam animi reiciendis doloremque eos provident eaque magnam recusandae, accusamus tempore molestias esse laboriosam enim deleniti id sit.</p>
+    </div>
+  )
+}
